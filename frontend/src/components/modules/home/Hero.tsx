a8d0aa01740902b704ca0c1d77a9c262e8181492
nasim0994 <nasimuddin0994@gmail.com>
2025-03-17 12:58:15 +0600

    hero add

--- frontend/src/components/modules/home/Hero.tsx
+++ frontend/src/components/modules/home/Hero.tsx
@@ -1,33 +1,49 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation } from "swiper/modules";
+import "swiper/css";
 import { Link } from "react-router-dom";
+
+const sliders = [
+  {
+    _id: 1,
+    url: "https://dhz27qxctdo0r.cloudfront.net/images/scale/2x/80ac214e11c07d7b9161f6c280eaa1ec.webp",
+    link: "#",
+  },
+  {
+    _id: 2,
+    url: "https://dhz27qxctdo0r.cloudfront.net/images/scale/2x/99fb8f996bd4211c4f918b27e04f61a7.webp",
+    link: "#",
+  },
+];
 
 export default function Hero() {
   return (
     <section className="pt-2 sm:pt-4">
       <div className="container">
-        <div className="grid grid-cols-1 gap-1.5 md:grid-cols-3 md:items-start md:gap-3">
-          <div className="h-40 sm:h-72 md:col-span-2 md:h-[300px] lg:h-[400px]">
-            <Link to="#">
-              <img
-                src="https://www.bmw-me.com/content/dam/bmw/marketMIDEAST/common/local-content/X%20Range%20Campaign/Xrange-home.jpg"
-                alt="banner"
-                className="h-full w-full object-cover"
-                loading="lazy"
-              />
-            </Link>
-          </div>
-
-          <div className="flex gap-2 md:flex-col">
-            <img
-              src="https://socar.my/blog/wp-content/uploads/2020/12/Phase-2_Blog.jpg"
-              alt="car-detailing"
-              className="w-[49.5%] object-cover md:h-[146px] md:w-full lg:h-[196px]"
-            />
-            <img
-              src="https://www.karrentacar.com.tr/dosya/2561/haber/23-2-special-car-rental-campaign-for-website-and-mobile-application_986.webp"
-              alt=".car-sale"
-              className="w-[49.3%] object-cover md:h-[146px] md:w-full lg:h-[195.5px]"
-            />
-          </div>
+        <div className="relative h-40 sm:h-80 lg:h-[450px]">
+          <Swiper
+            navigation={true}
+            modules={[Navigation, Autoplay]}
+            loop={true}
+            autoplay={{
+              delay: 5000,
+              disableOnInteraction: false,
+            }}
+            className="mySwiper h-full w-full rounded-2xl overflow-hidden"
+          >
+            {sliders?.map((banner) => (
+              <SwiperSlide key={banner._id}>
+                <Link to={banner?.link}>
+                  <img
+                    src={banner?.url}
+                    alt="banner"
+                    className="h-full w-full object-cover"
+                    loading="lazy"
+                  />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </section>
